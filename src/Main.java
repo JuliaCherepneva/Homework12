548317ebd1346d6077318fa685cac14364034812
JuliaCherepnev <yuliya.cherepnev@mail.ru>
2024-05-22 22:39:14 +0400

    Выполненное домашнее задание 4

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,17 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Book firstBook = new Book("Чистый код: создание, анализ и рефакторинг","Роберт Мартин",  2023);
-        System.out.println(firstBook.getBookTitle() + ". " + firstBook.getAuthor() + ", " + firstBook.getYearOfPublication());
-        Book secondBook = new Book("Java: руководство для начинающих", "Герберт Шилдт",  2023);
-        secondBook.setYearOfPublication(2022);
-        System.out.println(secondBook.getBookTitle() + ". " + secondBook.getAuthor() + ", " + secondBook.getYearOfPublication());
-
         Author firstAuthor = new Author("Роберт Мартин");
         System.out.println(firstAuthor.getName());
         Author secondAuthor = new Author("Герберт Шилдт");
         System.out.println(secondAuthor.getName());
+
+        Book firstBook = new Book("Чистый код: создание, анализ и рефакторинг",firstAuthor, 2023);
+        System.out.println(firstBook.getBookTitle() + ". " + firstBook.getAuthor().getName() + ", " + firstBook.getYearOfPublication());
+        Book secondBook = new Book("Java: руководство для начинающих", secondAuthor, 2023);
+        secondBook.setYearOfPublication(2022);
+        System.out.println(secondBook.getBookTitle() + ". " + secondBook.getAuthor().getName() + ", " + secondBook.getYearOfPublication());
+
+
     }
 }
