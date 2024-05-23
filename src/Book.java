46f944ebc9a95fc9128239f78a237ded87b90db8
JuliaCherepnev <yuliya.cherepnev@mail.ru>
2024-05-23 23:07:12 +0400

    Выполненное домашнее задание 1

--- src/Book.java
+++ src/Book.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Book {
     private final String bookTitle;
     private final Author author;
@@ -20,5 +22,24 @@ public class Book {
     }
     public void setYearOfPublication (int yearOfPublication) {
         this.yearOfPublication = yearOfPublication;
+    }
+
+    @Override
+    public String toString() {
+        return this.bookTitle + ". " + this.author + ", " + this.yearOfPublication;
+    }
+// Книги считаются одинаковыми, если совпадают автор и название, предположим, что год издания не имеет значения
+    @Override
+    public boolean equals(Object o) {
+        if (getClass() != o.getClass()) {
+            return false;
+        }
+        Book book = (Book) o;
+        return bookTitle.equals(book.bookTitle) && author.equals(book.author);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(bookTitle, author, yearOfPublication);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -3,16 +3,23 @@
 public class Main {
     public static void main(String[] args) {
         Author firstAuthor = new Author("Роберт Мартин");
-        System.out.println(firstAuthor.getName());
+        System.out.println(firstAuthor);
         Author secondAuthor = new Author("Герберт Шилдт");
-        System.out.println(secondAuthor.getName());
+        System.out.println(secondAuthor);
+
+        System.out.println(firstAuthor.equals(secondAuthor));
 
         Book firstBook = new Book("Чистый код: создание, анализ и рефакторинг",firstAuthor, 2023);
-        System.out.println(firstBook.getBookTitle() + ". " + firstBook.getAuthor().getName() + ", " + firstBook.getYearOfPublication());
+        System.out.println(firstBook);
         Book secondBook = new Book("Java: руководство для начинающих", secondAuthor, 2023);
         secondBook.setYearOfPublication(2022);
-        System.out.println(secondBook.getBookTitle() + ". " + secondBook.getAuthor().getName() + ", " + secondBook.getYearOfPublication());
+        System.out.println(secondBook);
+
+        System.out.println(firstBook.equals(secondBook));
+
 
 
     }
+
+
 }
